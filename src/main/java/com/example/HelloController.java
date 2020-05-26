f3fa7ba0a0242c470d75f1e3f4d5f7f2a142a922
corcelli <34511305+corcelli@users.noreply.github.com>
2020-05-26 16:18:11 -0300

    valor 5

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("")
     public String index() {
-        return "Greetings from Spring Boot Corcelli 4!";
+        return "Greetings from Spring Boot Corcelli 5!";
     }
 
 }
