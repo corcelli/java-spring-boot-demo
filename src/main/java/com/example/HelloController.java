bdd0d6597d898cc33e1e09c0dbcb77ec48209432
corcelli <tiagocorcelli@gmail.com>
2020-05-27 22:14:48 -0300

    tes

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("")
     public String index() {
-        return "Greetings from Spring Boot Corcelli V5!";
+        return "Greetings from Spring Boot Corcelli V6!";
     }
 
 }
